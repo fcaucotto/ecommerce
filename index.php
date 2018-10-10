128be84481012603440c827f7e705ab41d24b345
unknown <fcaucotto@yahoo.com.br>
2018-10-09 23:57:36 -0300

    inicio

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php 
- 
- echo "hello world";
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+
+});
+
+$app->run();
 
  ?>
